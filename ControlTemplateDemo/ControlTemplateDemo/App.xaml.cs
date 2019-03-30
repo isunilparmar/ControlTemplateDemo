da240197eb61653a492605c06949190841d3c40a
isunilparmar <parmarsunil175@gmail.com>
2019-03-30 10:28:49 -0700

    Initial Commit

--- ControlTemplateDemo/ControlTemplateDemo/App.xaml.cs
+++ ControlTemplateDemo/ControlTemplateDemo/App.xaml.cs
@@ -11,7 +11,7 @@ namespace ControlTemplateDemo
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new MainPage());
         }
 
         protected override void OnStart()
--- ControlTemplateDemo/ControlTemplateDemo/Page2.xaml.cs
+++ ControlTemplateDemo/ControlTemplateDemo/Page2.xaml.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace ControlTemplateDemo
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page2 : ContentPage
+	{
+		public Page2 ()
+		{
+			InitializeComponent ();
+		}
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            App.Current.MainPage.Navigation.PushAsync(new Page1());
+            App.Current.MainPage.Navigation.PushAsync(new Page3());
         }
     }
 }
